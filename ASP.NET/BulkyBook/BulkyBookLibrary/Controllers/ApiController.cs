f72e6ff0deb785e359d5f7465e341459fd313aa8
AdmiJW <jamweg22@gmail.com>
2022-01-08 03:04:57 +0800

    Added Bulky Book

---
+++ ASP.NET/BulkyBook/BulkyBookLibrary/Controllers/ApiController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BulkyBookLibrary.Data;
+using BulkyBookLibrary.Models;
+
+namespace BulkyBookLibrary.Controllers;
+
+
+
+[ApiController]                     // Specify that this is an API Controller
+[Route("[controller]")]             // The route for controller. [controller] will be replaced by controller name
+public class ApiController : ControllerBase         // We don't inherit from Controller, because Controller includes view specific implementation
+{
+    private readonly ApplicationDbContext _db;
+
+
+    public ApiController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+
+    [HttpGet("{id}")]
+    public ActionResult<Category> GetSingle(int id)
+    {
+        Category? cat = _db.Categories.Find(id);
+
+        if (cat == null) return NotFound();
+        return cat;
+    }
+
+    [HttpGet]
+    public IEnumerable<Category> Get()
+    {
+        return _db.Categories.ToList();
+    }
+
+
+    // This POST endpoint expects a Category object inside the body of HTTP request. Like JSON.
+    // Eg: { "name": "BookC", "displayOrder": 1 }
+    [HttpPost]
+    public IActionResult Create(Category category)
+    {
+        _db.Categories.Add(category);
+        _db.SaveChanges();
+        return CreatedAtAction(nameof(Create), new { id = category.Id }, category);
+    }
+
+
+
+    // This PUT endpoint expects a Category object inside the body of HTTP request. Like JSON.
+    // Eg: { "id": "1", "name": "BookC", "displayOrder": 1 }
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Category cat)
+    {
+        if (id != cat.Id) return BadRequest();
+
+        Category? categoryInDb = _db.Categories.Find(id);
+        if (categoryInDb is null) return NotFound();
+
+        categoryInDb.Name = cat.Name;
+        categoryInDb.DisplayOrder = cat.DisplayOrder;
+        categoryInDb.CreatedDateTime = cat.CreatedDateTime;
+
+        _db.SaveChanges();
+        return NoContent();
+    }
+
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        Category? cat = _db.Categories.Find(id);
+
+        if (cat is null) return NotFound();
+
+        _db.Categories.Remove(cat);
+        _db.SaveChanges();
+        return NoContent();
+    }
+
+}
---
+++ ASP.NET/BulkyBook/BulkyBookLibrary/Controllers/CategoryController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BulkyBookLibrary.Data;
+using BulkyBookLibrary.Models;
+
+namespace BulkyBookLibrary.Controllers;
+
+
+
+
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+
+
+    public IActionResult Index()
+    {
+        // Retrieve the list of Category from database, and pass into our view.
+        IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+        return View(objCategoryList);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0) return NotFound();
+
+        Category? categoryFromDb = _db.Categories.Find(id);
+        if (categoryFromDb == null) return NotFound();
+
+        return View(categoryFromDb);
+    }
+
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0) return NotFound();
+
+        Category? categoryFromDb = _db.Categories.Find(id);
+        if (categoryFromDb == null) return NotFound();
+
+        return View(categoryFromDb);
+    }
+
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        // Custom Validation - Done via ModelState.AddModelError
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("CustomError", "The Display order cannot exactly match the Name.");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("CustomError", "The Display Order cannot exactly match the Name.");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete_POST(int? id)
+    {
+        Category? category = _db.Categories.Find(id);
+        if (category == null) return NotFound();
+
+        _db.Categories.Remove(category);
+        _db.SaveChanges();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
+    }
+
+}
